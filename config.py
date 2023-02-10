64b570d253cdd26c014d97b06d3078157a3e0142
Ben Smith <bsmithnzl108@gmail.com>
2023-02-10 23:29:00 +1300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+
+import torch
+import torchvision
+
+BATCH_SIZE = 20
+DEVICE = torch.device('cpu')
+#DEVICE = torch.device('cuda')
+#DEVICE = torch.device('mps')
+EPOCHS = 1
+K = 5
+LEARNING_RATE = 0.001
+MOMENTUM = 0.9
+NUM_OF_WORKERS = 0
+SEED = 47
+TRANSFORM = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Resize(416),
+    torchvision.transforms.CenterCrop(384),
+    torchvision.transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+TOKEN = "dropbox token goes here"
